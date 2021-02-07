9e2b3c50aa4461cc3c0898544305be9742bf9c6f
anddreza <77905947+anddreza@users.noreply.github.com>
2021-02-07 15:43:49 -0400

    Última parte
    Terminei esse projeto que é pequeno, mas deu para aplicar alguns conceitos aprendidos.

--- aula 02/variavel atividade/var.js
+++ aula 02/variavel atividade/var.js
@@ -1,10 +1,10 @@
-let a = window.document.getElementById('fnum')
-let b = window.document.getElementById('flista')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
-function isNumero(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
@@ -12,23 +12,49 @@ function isNumero(n){
 }
 
 
-function inLista (n, 1) {
-    if (l.indexOf(Number(n)) != -1){
-        return false
+function inLista (n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
     } else {
         return false
     }
 }
 
-function adicionar(){
-    
+function adicionar() {
     if(isNumero(num.value) && !inLista (num.value, valores)){
         valores.push(Number(num.value))
-        let item = document.creatElement('option')
+        let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        window.alert('Vlores inválido ou já encontrado na lista.')
+        window.alert('Valores inválido ou já encontrado na lista.')
     } 
-    
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert(`Adicione valores antes de finalizar!`)
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores [0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            if (valores[pos] > maior )
+            maior = valores [pos]
+            if (valores[pos] < menor)
+            menor = valores [pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo , temos ${tot} números cadastrados </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>` 
+        res.innerHTML += `<p> Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${media}.</p>`
+    }
 }
